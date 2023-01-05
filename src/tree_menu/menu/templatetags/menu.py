c0fa83afb76504bd673e098f8ed63bc972849eaa
Ilya Verbilo <verbiloilyaa@gmail.com>
2023-01-06 01:21:24 +0300

    error handling while index request

--- src/tree_menu/menu/templatetags/menu.py
+++ src/tree_menu/menu/templatetags/menu.py
@@ -1,5 +1,6 @@
 from django import template
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 
 from tree_menu.menu.models import Menu
 
@@ -7,11 +8,15 @@ register = template.Library()
 
 
 @register.inclusion_tag('menu/menu.html', takes_context=True)
-def draw_menu(context, name):
+def draw_menu(context, name: str) -> dict:
     menu = get_object_or_404(Menu, name=name)
     absolute_uri = context.get('request').build_absolute_uri()
+    menu_context = {'menu': menu}
     if absolute_uri:
-        active_menu = Menu.objects.get(url=absolute_uri)
-        activated_menu_ids = [active_menu.id]
-        return {'active_menu_ids': activated_menu_ids + active_menu.get_parents_id(), 'menu': menu}
-    return {'menu': menu}
+        try:
+            active_menu = Menu.objects.get(url=absolute_uri)
+            activated_menu_ids = [active_menu.id] + active_menu.get_parents_id()
+        except ObjectDoesNotExist:
+            activated_menu_ids = []
+        menu_context.update({'active_menu_ids': activated_menu_ids})
+    return menu_context
